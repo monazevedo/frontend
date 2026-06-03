213e6796682b11fdf8b638f0ea89697f1b0121bf
monazevedo <monica.alex.azevedo@gmail.com>
2026-06-03 22:23:22 +0100

    Aula05 finalizada

---
+++ aula05/src/app/components/avatar/avatar.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  imports: [],
+  templateUrl: './avatar.html',
+  styleUrl: './avatar.css',
+})
+export class Avatar {
+logout() {
+throw new Error('Method not implemented.');
+}
+  localNome:string = "";
+  localImage!:string;
+
+}
